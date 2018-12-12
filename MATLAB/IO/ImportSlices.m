e8e49820bfb9b77005628958da785cda6b7a5bfb
Tim Van De Looverbosch <tim.vandelooverbosch@gmail.com>
2018-12-12 10:03:05 +0100

    Create function getsubdirs.m to easily get all subfolders is a certain directory. Updated the ImportSlices.m function to easily change the voxelsize of a CT scan when importing slices.

--- MATLAB/IO/ImportSlices.m
+++ MATLAB/IO/ImportSlices.m
@@ -1,29 +1,24 @@
-function [ scan ] = ImportSlices(imageLocation,SelectEveryNthImage)
-%% ImageLocation is a character containing the path for the images e.g., %'C:\Users\user1\Projects\project1\images\set1'
-% n: select slices per n-images: possibility to reduce number of slices to
-% be selected. If all slices must be used, do not specify "n", or set it to
-% 1.
+function [ image_stack ] = ImportSlices(input_path, current_voxel_size, target_voxel_size)
+% ImageLocation is a character containing the path for the images e.g., %'C:\Users\user1\Projects\project1\images\set1'
 
-%% If "n" is not given, take all slices
- if nargin == 1
-    SelectEveryNthImage=1;
- end
 
-%% Add images to image set
-Images=imageDatastore(imageLocation);
-%Images=imageSet(imageLocation,'recursive');
-scan=[];
+% Add images to image datastore
+Images = imageDatastore(input_path);
 
-%% make a subset of the slices
-% z-slices
-for i=1:SelectEveryNthImage:numel(Images.Files)
+% Loop over images and add them to the stack of slices
+image_stack = [];
+for i = 1:numel(Images.Files)
     slice = imread(Images.Files{i});
-    scan=cat(3,scan,slice);
+    image_stack = cat(3, image_stack, slice);
 end
-% same resolution for x and y
-scan = scan(1:SelectEveryNthImage:size(scan,1),1:SelectEveryNthImage:size(scan,2),:);
 
-%display
-%figure;imshow3D(scan)
+% Resize the image to the desired voxelsize
+if nargin > 1
+	current_size = size(image_stack);
+	true_size = current_size .* current_voxel_size;
+	target_size = true_size ./ target_voxel_size;
+	image_stack = imresize3(image_stack, target_size);
+end
+
 end
 
