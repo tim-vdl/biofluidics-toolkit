699829b954c9f47de2fcc0e735409cd338b0f4b7
Tim Van De Looverbosch <u0117721@kuleuven.be>
2019-06-26 18:03:09 +0200

    Add functionality for colored volumes and images

--- MATLAB/Visualization/show_ortho_slices.m
+++ MATLAB/Visualization/show_ortho_slices.m
@@ -1,21 +1,38 @@
-function show_ortho_slices(sample)
+function show_ortho_slices(sample, varargin)
+
+valid_orientations = {'vertical', 'horizontal'};
+p = inputParser;
+addParameter(p, 'Orientation', 'vertical', @(x) any(validatestring(x, valid_orientations)));
+addParameter(p, 'FontSize', 20, @isnumeric);
+parse(p, varargin{:});
+
 volume_size = size(sample);
 rounded_mids = round(volume_size/2);
+slice_xy = squeeze(sample(:,:,rounded_mids(3),:));
+slice_xz = squeeze(sample(:,rounded_mids(2),:,:));
+slice_yz = squeeze(sample(rounded_mids(1),:,:,:));
 
-slice_xy = sample(:,:,rounded_mids(3));
-slice_xz = squeeze(sample(:,rounded_mids(2),:));
-slice_yz = squeeze(sample(rounded_mids(1),:,:));
+% Define grid of subplots
+if strcmp(p.Results.Orientation, valid_orientations{1})
+    i = 3;
+    j = 1;
+else
+    i = 1;
+    j = 3;
+end
 
+% Make plot
+font_size = p.Results.FontSize;
 figure;
-subplot(3,1,1)
+subplot(i,j,1)
 imshow(slice_xy)
-title('XY slice')
-subplot(3,1,2)
+title('XY slice', 'FontSize', font_size)
+subplot(i,j,2)
 imshow(slice_xz)
-title('XZ slice')
-subplot(3,1,3)
+title('XZ slice', 'FontSize', font_size)
+subplot(i,j,3)
 imshow(slice_yz)
-title('YZ slice')
+title('YZ slice', 'FontSize', font_size)
 
 end
 
