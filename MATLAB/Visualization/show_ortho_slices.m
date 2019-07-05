0bd26525b9a4c7ffe0ad9a18a1c97df6f65b2bf7
Tim Van De Looverbosch <u0117721@kuleuven.be>
2019-07-05 10:52:36 +0200

    Add feature to make the plot at any slice in stead of in the middle

--- MATLAB/Visualization/show_ortho_slices.m
+++ MATLAB/Visualization/show_ortho_slices.m
@@ -3,14 +3,16 @@ function show_ortho_slices(sample, varargin)
 valid_orientations = {'vertical', 'horizontal'};
 p = inputParser;
 addParameter(p, 'Orientation', 'vertical', @(x) any(validatestring(x, valid_orientations)));
+addParameter(p, 'Location', [0.5, 0.5, 0.5]);
 addParameter(p, 'FontSize', 20, @isnumeric);
 parse(p, varargin{:});
 
 volume_size = size(sample);
-rounded_mids = round(volume_size/2);
-slice_xy = squeeze(sample(:,:,rounded_mids(3),:));
-slice_xz = squeeze(sample(:,rounded_mids(2),:,:));
-slice_yz = squeeze(sample(rounded_mids(1),:,:,:));
+locations = round(volume_size .* p.Results.Location);
+
+slice_xy = squeeze(sample(:,:,locations(3),:));
+slice_xz = squeeze(sample(:,locations(2),:,:));
+slice_yz = squeeze(sample(locations(1),:,:,:));
 
 % Define grid of subplots
 if strcmp(p.Results.Orientation, valid_orientations{1})
