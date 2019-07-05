1221fef32a67cedb2f693b90dccf2a90daf1bf3d
Tim Van De Looverbosch <u0117721@kuleuven.be>
2019-07-05 10:53:31 +0200

    Also provide file names without extension

--- MATLAB/IO/get_folder_file_names.m
+++ MATLAB/IO/get_folder_file_names.m
@@ -1,7 +1,9 @@
-function [file_names,file_paths, number_of_files] = get_folder_file_names(input_path, file_extension)
+function [file_names_with_extension, file_names, file_paths, number_of_files] = get_folder_file_names(input_path, file_extension)
 files = dir(fullfile(input_path, file_extension));
-file_names = {files(:).name};
-file_paths = fullfile(input_path, file_names);
-number_of_files = numel(file_names);
+file_names_with_extension = {files.name};
+extenstion = file_extension(2:end);
+file_names = erase(file_names_with_extension, extenstion);
+file_paths = fullfile(input_path, file_names_with_extension);
+number_of_files = numel(file_names_with_extension);
 end
 
