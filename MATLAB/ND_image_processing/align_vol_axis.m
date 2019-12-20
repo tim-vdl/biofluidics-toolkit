6ae1841400d3cc1bc510e70df0d7e835dd401b62
Tim Van De Looverbosch <u0117721@kuleuven.be>
2019-12-20 14:22:01 +0100

    Initial commit

---
+++ MATLAB/ND_image_processing/align_vol_axis.m
@@ -0,0 +1,15 @@
+function [rotated_volume, angle, rotation_axis] = align_vol_axis(binary_volume, target_axis)
+%ALIGN_VOL_AXIS Align principal axis of a binary volume with a target axis.
+% target axis: 3x1 vector
+assert(isequal(size(target_axis),[3,1]), 'Target axis should have size [3, 1]')
+stats = regionprops3(binary_volume, 'Centroid', 'EigenVectors');
+eigenvects = stats.EigenVectors{:};
+principal_axis = [eigenvects(2,1); eigenvects(1,1); eigenvects(3,1)]; % image space to XYZ
+
+rotation_axis = cross(principal_axis,target_axis);
+angle = -atand(norm(cross(principal_axis,target_axis),...
+    dot(principal_axis,target_axis)));
+
+rotated_volume = logical(imrotate3(double(binary_volume), angle, rotation_axis'));
+end
+
