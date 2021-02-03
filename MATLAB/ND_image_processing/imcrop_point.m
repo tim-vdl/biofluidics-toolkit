270329438aed105fee4620d307af60fe44aefaa6
Tim Van De Looverbosch <tim.vandelooverbosch@gmail.com>
2021-02-03 16:15:37 +0100

    Remove thresholding on grayscale image. Functions now only work on binary images to be more generalistic to other workflows of obtaining the binary image

--- MATLAB/ND_image_processing/imcrop_point.m
+++ MATLAB/ND_image_processing/imcrop_point.m
@@ -1,15 +1,6 @@
-function [cropped_img, rect] = imcrop_point(img, point, dims, bright_foreground)
+function [cropped_img, rect] = imcrop_point(img, point, dims)
 %IMCROP_POINT crops out an area around a given point ([col, row]). The size of the
-%cropped image in each dimension must be defined by 'dims'. Define if the
-%foreground is bright ('bright_foreground' = 1) or dark
-%('bright_foreground' = 0) so that the centroid is calculated on the
-%foreground pixels.
-
-if bright_foreground
-    bw = img > multithresh(img, 1);
-else
-    bw = img < multithresh(img, 1);
-end
+%cropped image in each dimension must be defined by 'dims'.
 
 rect = [point(1) - dims(1)/2,...
     point(2) - dims(2)/2,...
@@ -17,5 +8,4 @@ rect = [point(1) - dims(1)/2,...
     dims(2)];
 cropped_img = imcrop(img, rect);
     
-end
 
